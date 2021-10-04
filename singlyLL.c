ca75f1cbc2d7dd2097f74849e8d0f4e82662e4da
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-10-04 12:03:15 +0530

    create singlyLL.c file

---
+++ singlyLL.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Structure defination must be global
+typedef struct Node {
+    int data;
+    // address of the next node
+    Node *next;
+}Node;
+
+int main(int argc, char *argv[])
+{
+    // Head Pointer
+    Node *head = NULL;
+    return 0;
+}
