538202ca49a168753d855a86a419307ab439578f
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-10-16 11:54:44 +0530

    Add comments to the queue file

--- Queue/queue.c
+++ Queue/queue.c
@@ -9,8 +9,6 @@
 
 /**
  * Dynamic Implementation of Queue:
- * Front will always point to the front node of SLL
- * Rear will always point to the last node of SLL
  */
 typedef struct Queue {
     int info; 
@@ -22,9 +20,19 @@ void Insert(Queue **, Queue **, int);
 
 int main(int argc, char *argv[])
 {
+    /**
+     * Front will always point to the front node of SLL
+     * Rear will always point to the last node of SLL
+     */ 
     Queue *front, *rear;
+
+    // Queue initialization
     InitQueue(&front, &rear);
+
+    // Insertion at rear
     Insert(&front, &rear, 10);
+
+    // testing
     printf("%d\n", front->info);
     return 0;
 }
@@ -33,14 +41,38 @@ void InitQueue(Queue **front_ref, Queue **rear_ref) {
     (*front_ref) = (*rear_ref) = NULL;
 }
 
-void Insert(Queue **front_ref, Queue **rear_ref, int value) {
+/**
+ * Given two reference (pointer to pointer)
+ * front and rear and a value, add to 
+ * the end of the queue
+ */ 
+void Insert(Queue **front_ref, 
+            Queue **rear_ref, int value) {
+
+    // Allocate memory for the Queue element
     Queue *new = (Queue *)malloc(sizeof(Queue));
+
+    // Set the value
     new->info = value;
+
+    // Rear->next should be NULL
     new->next = NULL;
+
+    /**
+     * If the Queue is empty then both 
+     * front and rear pointers will be
+     * pointing to the same Queue element
+     */ 
     if((*front_ref) == NULL && (*rear_ref) == NULL) {
         *front_ref = *rear_ref = new;
         return;
     } 
+
+    // Add the new element to the
+    // end of the queue
     (*rear_ref)->next = new;
+
+    // Fix the position of rear pointer
+    // to the end of the queue
     (*rear_ref) = (*rear_ref)->next;
 }
