00ecb5b00b3c75517742caa114ca006dc16b41db
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-10-21 14:01:17 +0530

    create priorityQ.c file
    Add explanation upon ascending and descending priority Queue

---
+++ Queue/priorityQ.c
@@ -0,0 +1,16 @@
+/** 
+ ** Ascending Prority Queue
+ * Queue in which insertions can happens arbitarily, but delete
+ * operation always deletes the element with smallest priority.
+ *
+ ** Decending Prority Queue
+ * Queue in which insertions can happensarbitarily, but delete
+ * operation always deletes the element with largest priority
+ *
+ *? With every element stored in the queue, there is
+ *? a priority value associated to it
+ * 
+ * In Multiprogramming Operating Systems "nice" system call 
+ * is used to increase/decrease the priority of the program
+ * https://www.tutorialspoint.com/unix_system_calls/nice.htm
+ */ 
