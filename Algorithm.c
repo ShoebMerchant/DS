d3442e5f7706e683dc40724c0ddaf242f62f7193
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-10-22 11:51:04 +0530

    create Algorithm.c file Add notes about algorithms

---
+++ Algorithm.c
@@ -0,0 +1,33 @@
+/**
+ * It is a finite sequence of instruction, 
+ * which when followed, 
+ * accomplishes a particular task. 
+ *--* 
+ *? Desirabel properties of algorithm:
+ ** Input ->
+    zero or more inputs are externally supplied
+ ** Output ->
+    At least one quantity is produced
+ ** Finiteness -> 
+    No matter what, the algo must terminate 
+    after finite amount of time.
+ ** Definiteness -> 
+    Each instruction of the algorithm must 
+    be precise and unambiguous
+ ** Effectiveness -> 
+    Every instruction must be so 
+    basic that it can be carried out 
+    by a person with  paper and pencil.
+ *--*   
+ *? Algorithm Complexity -> 
+ ** Space Complexity: 
+    Amount of computer memory it takes during it's execution
+ ** Time Complexity: 
+    Amount of computer time it takes during it's execution
+ *--*
+ ** Instance-Independent Algorithms ->
+    Have time complexity of O(1)
+ ** Instance-Dependent Algorithms
+    
+ *
+ */
